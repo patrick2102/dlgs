d002fbd51d4b034de6bc7ba8995f45f5b80aad53
paco <paco@itu.dk>
2022-10-06 11:31:21 +0200

    lab 5 cleanup

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -16,30 +16,15 @@ import torch.nn.functional as F
 from collections import deque  # for memory
 from tqdm import tqdm          # for progress bar
 
-
-env = gym.make('CartPole-v1', render_mode='human')
-"""
-for _ in tqdm(range(10)):
-    state, _ = env.reset()
-    done = False
-    while not done:
-        action = env.action_space.sample()
-        next_state, reward, done, _, _ = env.step(action)
-
-"""
-env.close()
-
-
-
 #Model:
 class Model(nn.Module):
     def __init__(self, observation_size, action_size):
         super(Model, self).__init__()
-        self.dense1 = nn.Linear(observation_size, 32)
+        self.dense1 = nn.Linear(observation_size, 100)
         torch.nn.init.xavier_uniform_(self.dense1.weight)
-        self.dense2 = nn.Linear(32, 32)
+        self.dense2 = nn.Linear(100, 100)
         torch.nn.init.xavier_uniform_(self.dense2.weight)
-        self.dense3 = nn.Linear(32, action_size)
+        self.dense3 = nn.Linear(100, action_size)
         torch.nn.init.xavier_uniform_(self.dense3.weight)
 
     def forward(self, x):
@@ -48,13 +33,12 @@ class Model(nn.Module):
         x = self.dense2(x)
         x = F.relu(x)
         x = self.dense3(x)
-        x = F.relu(x)
         return x
 
-    def predict(self, x):              # actually make a prediction
+    def predict(self, x):
         x = torch.tensor(x)
-        x = self.forward(x)            # send x through neural net
-        return torch.argmax(x)  # predict most likely thing
+        x = self.forward(x)
+        return torch.argmax(x) 
 
 class Agent:
     def __init__(self, observation_size, action_size):
@@ -63,8 +47,8 @@ class Agent:
         self.criterion = nn.MSELoss()
         self.model = Model(observation_size, action_size)
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
-        self.N = 4000
-        self.explore_rate = 0.0
+        self.N = 2000
+        self.explore_rate = 0.15
         self.explore_decay = 0.99
         self.explore_min = 0.0
         self.discount_rate = 0.9
@@ -81,14 +65,11 @@ class Agent:
             return random.randint(0, 1)
 
         action = self.model.predict(state)
-        #action = torch.argmax(action)
         action = int(action)
-
         return action
 
     def replay(self, batch_size):
 
-        total_loss = 0
         minibatch = random.sample(self.memory, batch_size)
         self.optimizer.zero_grad()
 
@@ -108,13 +89,7 @@ class Agent:
         else:
             v = r
         pred = self.model.forward(s)[a]
-        #print("before")
-        #print("s:", s)
-        #print("a:", a)
-        #print("pred:", pred)
-        #print("v:", v)
         loss = self.criterion(pred, v)
-        #print("after")
         loss.backward()
 
 
@@ -133,7 +108,6 @@ def train(env, agent, episodes=10000, batch_size=64):  # train for many games
         state, _ = env.reset()
         done = False
         total_r = 0
-        total_loss = 0
         iter = 0
         while not done:
             # 1. make a move in game.
@@ -156,9 +130,8 @@ def train(env, agent, episodes=10000, batch_size=64):  # train for many games
             if len(agent.memory) >= batch_size:
                 agent.replay(batch_size)
             iter += 1
-        print("score: ", total_r)
-        #print("avg loss: ", total_loss/iter)
-        print("explore: ", agent.explore_rate)
+        #print("score: ", total_r)
+        #print("explore: ", agent.explore_rate)
 
         # plotting
         x.append(e)
@@ -169,42 +142,20 @@ def train(env, agent, episodes=10000, batch_size=64):  # train for many games
 
             plt.draw()
             plt.pause(0.1)
-
-        #if e % 100 == 0 and e != 0:
-            #print("Saving model...")
-            #torch.save(agent.model.state_dict(), 'model3.pth')
-        """
-        if len(x) == 100:
-            temp_x = []
-            temp_y = []
-            i = 0
-            while i < len(x):
-                avg = 0
-                for _ in range(10):
-                    avg += x[i]
-                    i += 1
-                avg /= 10
-                temp_x.append(avg)
-                temp_y.append(y[i])
-            x = temp_x
-            y = temp_y
-        """
-
-        
     env.close()
 
 
-env = gym.make('CartPole-v1')#, render_mode='human')  # , render_mode='human')
+env = gym.make('CartPole-v1' , render_mode='human')
 agent = Agent(env.observation_space.shape[0], env.action_space.n)
 #agent.model.state_dict = torch.load('model3.pth')
 train(env, agent)
-torch.save(agent.model.state_dict(), 'model1.pth')
+torch.save(agent.model.state_dict(), 'model3.pth')
 
 
 #torch.load('model2.pth')
 #agent = Agent(env.observation_space.shape[0], env.action_space.n)
 
-agent.model.load_state_dict(torch.load('model1.pth'))
+agent.model.load_state_dict(torch.load('model2.pth'))
 
 #print(agent.model.state_dict)
 
